2b2a9562e6e73d20f2172f21071bdce176e920d1
Nhat_Truong <tranhuunhattruong2000@gmail.com>
2024-03-31 14:40:00 +0700

    delete project

--- Cross Site Request Forgery (XSRF) Attacks/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Cross_Site_Request_Forgery__XSRF__Attacks.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
